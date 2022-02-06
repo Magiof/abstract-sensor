6c939ee966761e7588f41197b3feaa4ad1783875
unknown <tjddnjs7559@gmail.com>
2022-02-06 10:37:51 +0900

    gazaa

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-class Sensor {}
+class Sensor {
+    constructor() {
+        this.powerStatus = 'off';
+        this.status;
+        this.reportingInterval = 10000;
+    }
+    turn(pwstatus) {
+        if (this.powerStatus === 'off') {
+            if (pwstatus === 'on') {
+                this.powerStatus = 'on';
+                this.status = 'idle';
+            }
+        } else if (this.powerStatus === 'on') {
+            if (pwstatus === 'on') {
+                throw new Error('이미 켜져 있습니다!');
+            } else if (pwstatus === 'off') {
+                this.powerStatus = 'off';
+            }
+        }
+    }
+    advanceTimersByTime(waitingTime) {
+        this.status = 'sensingDistance';
+        setTimeout('', waitingTime);
+        this.status = 'sensingDistance';
+    }
+}
 
 class IotServer {}
 
